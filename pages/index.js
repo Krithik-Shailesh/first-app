e9ee1f4c6ee9cda4ed509be47dc975368001e975
Krithik-Shailesh <krithik@param.network>
2022-01-23 22:27:42 +0530

    designed the login-page

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,52 @@ import signInStyles from '../styles/Signin.module.css'
 
 export default function Home() {
   const route = useRouter();
+
+  const verify = async (event) => {
+    const username = event.target.username.value
+    const password = event.target.password.value
+
+    console.log(username+ "   " + password)
+
+    route.push('/home')
+  }
+
   return (
-    
     <div className={signInStyles.primaryContainer}>
-      Login Page
-      <div>
-        <button onClick={() => {route.push('/verifyotp')}}>
-        Login
-        </button>
+      <div className="max-w-xs my-2 overflow-hidden rounded shadow-lg">
+      <div className="px-6 py-4 justify-between"> 
+        <div className="mb-2 text-xl font-bold text-center">Login</div>
+        <form className="flex flex-col">
+          <input
+            className="mb-4 border-b-2"
+            id="username"
+            name="username"
+            type="text"
+            placeholder='Username'
+            autoComplete="username"
+            required
+          />
+          <input
+            className="mb-4 border-b-2"
+            id="password"
+            name="password"
+            placeholder='Password'
+            type="password"
+            autoComplete="password"
+            required
+          />
+          <button
+            type="submit"
+            className="px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700"
+            onClick={verify}
+          >
+            Submit
+          </button>
+        </form>
       </div>
-    </div>
+      </div>
+      </div>
+      
 
   )
 }
