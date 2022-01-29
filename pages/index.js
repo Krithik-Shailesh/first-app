d3978454fa572bc2bd54354077e16c53cd1a0e65
Krithik-Shailesh <krithik@param.network>
2022-01-29 16:32:51 +0530

    added username, password changes

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,20 @@
-import { route } from 'next/dist/server/router'
-import Head from 'next/head'
+import { useState } from 'react';
 import { useRouter } from 'next/router'
 import signInStyles from '../styles/Signin.module.css'
 
 export default function Home() {
   const route = useRouter();
+  const [username, setUserName] = useState('');
+  const [password, setPassword] = useState('');
 
-  const verify = async (event) => {
-    const username = event.target.username.value
-    const password = event.target.password.value
 
-    console.log(username+ "   " + password)
-
+  const handleSubmit = e =>{
+    e.preventDefault();
+    const data = {
+      username,
+      password
+    }
+    console.log(data)
     route.push('/home')
   }
 
@@ -20,12 +23,13 @@ export default function Home() {
       <div className="max-w-xs my-2 overflow-hidden rounded shadow-lg">
       <div className="px-6 py-4 justify-between"> 
         <div className="mb-2 text-xl font-bold text-center">Login</div>
-        <form className="flex flex-col">
+        <form onSubmit={handleSubmit} className="flex flex-col">
           <input
             className="mb-4 border-b-2"
             id="username"
             name="username"
             type="text"
+            onChange={e => setUserName(e.target.value)}
             placeholder='Username'
             autoComplete="username"
             required
@@ -34,6 +38,7 @@ export default function Home() {
             className="mb-4 border-b-2"
             id="password"
             name="password"
+            onChange={e => setPassword(e.target.value)}
             placeholder='Password'
             type="password"
             autoComplete="password"
@@ -42,7 +47,7 @@ export default function Home() {
           <button
             type="submit"
             className="px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700"
-            onClick={verify}
+            
           >
             Submit
           </button>
