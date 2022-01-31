74b5e93eccc9de98cc83df355d9ffa1e97365c4d
Krithik-Shailesh <krithik@param.network>
2022-01-31 12:43:01 +0530

    added css changes to the login form

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router'
 import signInStyles from '../styles/Signin.module.css'
+import Link from 'next/link';
 
 export default function Home() {
   const route = useRouter();
@@ -22,10 +23,10 @@ export default function Home() {
     <div className={signInStyles.primaryContainer}>
       <div className="max-w-xs my-2 overflow-hidden rounded shadow-lg">
       <div className="px-6 py-4 justify-between"> 
-        <div className="mb-2 text-xl font-bold text-center">Login</div>
+        <div className="mb-6 text-xl font-bold text-center">Login</div>
         <form onSubmit={handleSubmit} className="flex flex-col">
           <input
-            className="mb-4 border-b-2"
+            className="mb-5 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="username"
             name="username"
             type="text"
@@ -35,7 +36,7 @@ export default function Home() {
             required
           />
           <input
-            className="mb-4 border-b-2"
+            className="mb-7 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="password"
             name="password"
             onChange={e => setPassword(e.target.value)}
@@ -46,11 +47,14 @@ export default function Home() {
           />
           <button
             type="submit"
-            className="px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700"
+            className="mb-6 px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700"
             
           >
             Submit
           </button>
+          <div className='mb-5 text-center'>New User?   
+          <Link href={'/signup'} ><a className='ml-3 text-cyan-400'>Create Account</a></Link>
+          </div>
         </form>
       </div>
       </div>
---
+++ pages/signup.js
@@ -0,0 +1,12 @@
+import signInStyles from '../styles/Signin.module.css'
+
+
+const signup = () => {
+    return(
+        <div className={signInStyles.primaryContainer}>
+            Sign Up
+        </div>
+    );
+}
+
+export default signup;
